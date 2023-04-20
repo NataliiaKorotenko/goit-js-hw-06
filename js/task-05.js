e645180a51ee9fd25cf69975f2efe16331724486
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2023-04-20 11:33:32 +0200

    task 6

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const nameInput = document.querySelector ('#name-input');
-const name Output = document.querySelector ('#name-output');
+const nameOutput = document.querySelector ('#name-output');
 
-nameInput.addEventListener ( 'input', (event) => {
+nameInput.addEventListener ('input', (event) => {
     const inputText = event.target.value;
-    nameOutput.textContent === '' ? 'Anonymous' .inputText,
+    nameOutput.textContent === '' ? 'Anonymous': inputText;
 });
---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.getElementById('validation-input');
+
+input.addEventListener ('blur', () => {
+    const inputLength = input.value.length;
+    const quantityLength = parseInt(input.dataset.length);
+
+    if (inputLength === quantityLength) {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+      } else {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+    }
+    });
