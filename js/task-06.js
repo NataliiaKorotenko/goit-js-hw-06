c415a3c869b0b7ec34a3c71e6174109427e294b2
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2023-04-21 15:04:46 +0200

    зміни

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,14 @@
-const input = document.querySelector('validation-input');
+const inputText = document.querySelector('#validation-input');
 
 input.addEventListener('blur', () => {
-  const inputLength = input.value.length;
-  const quantityLength = parseInt(input.dataset.length);
+  const inputLength = inputText.value.length;
+  const quantityLength = parseInt(inputText.dataset.length);
 
     if (inputLength === quantityLength) {
-        input.classList.remove('invalid');
-        input.classList.add('valid');
+      inputText.classList.remove('invalid');
+      inputText.classList.add('valid');
       } else {
-        input.classList.remove('valid');
-        input.classList.add('invalid');
+        inputText.classList.remove('valid');
+        inputText.classList.add('invalid');
     }
     });
