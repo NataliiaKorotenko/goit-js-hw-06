85c24796271a66abb3836d5aaa689cb446ad3941
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2023-04-21 14:56:55 +0200

    виправлення

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
-const input = document.getElementById('#validation-input');
+const input = document.querySelector('validation-input');
 
-input.addEventListener ('blur', () => {
-    const inputLength = input.value.length;
-    const quantityLength = parseInt(input.dataset.length);
+input.addEventListener('blur', () => {
+  const inputLength = input.value.length;
+  const quantityLength = parseInt(input.dataset.length);
 
     if (inputLength === quantityLength) {
         input.classList.remove('invalid');
