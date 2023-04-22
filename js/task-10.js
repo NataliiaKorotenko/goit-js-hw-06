2480a44d9ae0bf52b404a41387ae3b29004bb32b
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2023-04-22 21:04:26 +0200

    виправлення у task 10

--- js/task-10.js
+++ js/task-10.js
@@ -13,19 +13,27 @@ btnCreate.addEventListener('click', () => createBoxes(inputAmount.value));
 btnDestroy.addEventListener('click', destroyBoxes);
 
 const createBoxes = (amount) => {
+
+  if (isNaN(amount) || amount < 1 || amount > 100) {
+    alert('Введіть коректне число від 1 до 100');
+    return;
+  }
+
+  boxesContainer.textContent = '';
+
   for (let i = 0; i < amount; i++) {
     const div = document.createElement('div');
-    div.setAttribute('style', `
-      background-color: ${getRandomHexColor()};
-      width: ${30 + i * 10}px;
-      height: ${30 + i * 10}px;
-      margin-right: 10px;
-      margin-bottom: 10px;
-    `);
+    div.classList.add('box');
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.marginRight = '10px';
+    div.style.marginBottom = '10px';
     boxesContainer.appendChild(div);
   }
 }
 
 const destroyBoxes = () => {
-  boxesContainer.innerHTML = '';
+
+  boxesContainer.textContent = '';
 };
