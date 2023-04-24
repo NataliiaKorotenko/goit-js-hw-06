7eb1d9c81a4f6af301827d1a4edc2b378de5134c
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2023-04-24 13:38:49 +0200

    коригування

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
-const nameInput = document.querySelector ('#name-input');
-const nameOutput = document.querySelector ('#name-output');
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
 
-nameInput.addEventListener ('input', (event) => {
+nameInput.addEventListener('input', (event) => {
     const inputText = event.target.value.trim();
-    nameOutput.textContent === '' ? 'Anonymous': inputText;
+    nameOutput.textContent = inputText === '' ? 'Anonymous' : inputText;
 });
